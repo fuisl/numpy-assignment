bc2ed594de09cbd9c47fe0ca5500887edaa8fb29
fuisl <135719107+fuisl@users.noreply.github.com>
2025-03-15 09:19:00 +0700

    fix yaml.

---
+++ exercises.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def exercise_1():
+    # What is 1 + 1
+    return None
+
+
+# In[ ]:
+
+
+def exercise_2():
+    return None
+
+
+# In[ ]:
+
+
+def exercise_3():
+    return None
+
+
+# In[ ]:
+
+
+def exercise_4():
+    return None
+
+
+# In[ ]:
+
+
+def exercise_5():
+    return None
+
